7a5880cc0adb38a6ae51dd1d930fdd61ac2fef17
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-22 18:20:59 +0100

    Gesture Recognizer created

--- GestureRecognizerProject/ViewController.swift
+++ GestureRecognizerProject/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var CityLabel: UILabel!
+    
+    var Ankara = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        ImageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        
+        ImageView.addGestureRecognizer(gestureRecognizer)
 
+
+    }
+    @objc func changeImage() {
+        
+        
+        if Ankara == false {
+            ImageView.image = UIImage(named: "Ankara")
+            CityLabel.text = "Ankara"
+            Ankara = true
+            
+        }else {
+            ImageView.image = UIImage(named: "Istanbul")
+            CityLabel.text = "Istanbul"
+            Ankara = false
+        }
+        
+        
+    }
 
 }
 
