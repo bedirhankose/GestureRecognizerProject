3787c3b62305b59de71163466a82a12ff2303424
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-22 17:43:24 +0100

    Initial Commit

---
+++ GestureRecognizerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerProject
+//
+//  Created by Bedirhan Köse on 22.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
